9e8ef5f7454d60c6bf770e1b1e8d6bf204eda740
Maëlys Deschaux <maelys.dcb@gmail.com>
2023-04-02 18:03:43 +0200

    adding news functionalities

--- index.js
+++ index.js
@@ -9,14 +9,62 @@ let nbPotionStock = smallPotion + bigPotion;
 // HISTORIC LOGGER
 let logger = document.querySelector(".log");
 
-function playerLog(message) {
+function log(message) {
   logger.innerHTML = message + logger.innerHTML;
-  logger.style.color = "#D3E876";
 }
 
-function enemyLog(message) {
-  logger.innerHTML = message + logger.innerHTML;
-  logger.style.color = "#F0542D";
+// AI - GAME AUTOMATISATION
+function enemyAttacks(random) {
+  let attacks = ["punch", "sword", "fireball"];
+  random = attacks[Math.floor(Math.random() * attacks.length)];
+  if (enemyLife > 0) {
+    switch (random) {
+      case "punch":
+        playerLife -= 10;
+        log(
+          `<pre style='color:#F0542D;'>Enemy PUNCH ! Your life is now at <strong>${playerLife}hp</strong></pre>`
+        );
+        break;
+      case "sword":
+        playerLife -= 20;
+        log(
+          `<pre style='color:#F0542D;'>Enemy SWING ! Your life is now at <strong>${playerLife}hp</strong></pre>`
+        );
+        break;
+      case "fireball":
+        playerLife = Math.floor(playerLife / 2);
+        log(
+          `<pre style='color:#F0542D;'>Enemy FIREBALL ! Your life is now at <strong>${playerLife}hp</strong></pre>`
+        );
+        break;
+    }
+  }
+  updateLife();
+}
+
+// Function TURN TO ENEMY
+function turnToEnemy() {
+  if (enemyLife > 0) {
+    setTimeout(function () {
+      enemyAttacks(), disableButtons();
+    }, 600);
+  } else {
+    reset();
+  }
+}
+
+// DISABLED BUTTONS
+let actionButtons = document.querySelectorAll("button");
+function disableButtons() {
+  for (let i = 0; i < actionButtons.length; i++) {
+    if (actionButtons[i].disabled) {
+      actionButtons[i].disabled = false;
+      actionButtons[i].style.backgroundColor = "";
+    } else {
+      actionButtons[i].disabled = true;
+      actionButtons[i].style.backgroundColor = "rgba(180, 180, 180, 0.3)";
+    }
+  }
 }
 
 // ATTACK PUNCH
@@ -31,25 +79,27 @@ function attackWithHands() {
     updateLife();
     return true;
   } else {
-    playerLog(
-      `<pre>PUNCH ! Enemy life is now at <strong>${enemyLife} hp</strong></pre>`
+    log(
+      `<pre style='color:#d3e876;'>PUNCH ! Enemy life is now at <strong>${enemyLife}hp</strong></pre>`
     );
   }
   updateLife();
+  turnToEnemy();
+  disableButtons();
   return false;
 }
 
 // COMBO ATTACK X3
-function comboAttack(nbHits) {
-  let isEnemyDead = false;
-  let combo = document.getElementById("combo");
-  combo.play();
-  do {
-    playerLog(`COMBO X3`);
-    isEnemyDead = attackWithHands();
-    nbHits--;
-  } while (nbHits > 0 && !isEnemyDead);
-}
+// function comboAttack(nbHits) {
+//   let isEnemyDead = false;
+//   let combo = document.getElementById("combo");
+//   combo.play();
+//   do {
+//     log(`<pre style='color:#d3e876;'>COMBO X3</pre>`);
+//     isEnemyDead = attackWithHands();
+//     nbHits--;
+//   } while (nbHits > 0 && !isEnemyDead);
+// }
 
 // ATTACK WITH SWORD
 function attackWithSword() {
@@ -59,12 +109,13 @@ function attackWithSword() {
   if (enemyLife <= 0) {
     enemyLife = 0;
     reset();
-  } else {
-    playerLog(
-      `<pre>SWING ! Enemy life is now at <strong>${enemyLife} hp</strong></pre>`
-    );
   }
+  log(
+    `<pre style='color:#d3e876;'>SWING ! Enemy life is now at <strong>${enemyLife} hp</strong></pre>`
+  );
   updateLife();
+  turnToEnemy();
+  disableButtons();
 }
 
 // FIREBALL
@@ -75,44 +126,48 @@ function fireBall() {
   if (enemyLife <= 0) {
     reset();
   } else {
-    playerLog(
-      `<pre>Incredible ! Enemy's life is now at <strong>${enemyLife} hp</strong></pre>`
+    log(
+      `<pre style='color:#d3e876;'>Incredible ! Enemy's life is now at <strong>${enemyLife} hp</strong></pre>`
     );
   }
   updateLife();
+  turnToEnemy();
+  disableButtons();
 }
 
 // ENEMY ATTACK
-function enemyAttack() {
-  playerLife -= 40;
-  let playerGrunts = document.getElementById("playerGrunts");
-  playerGrunts.play();
-  if (playerLife <= 0) {
-    playerLife = 0;
-    reset();
-  } else {
-    enemyLog(
-      `<pre>Enemy attacked ! Your life is now at <strong>${playerLife} hp</strong></pre>`
-    );
-  }
-  updateLife();
-}
+// function enemyAttack() {
+//   playerLife -= 40;
+//   let playerGrunts = document.getElementById("playerGrunts");
+//   playerGrunts.play();
+//   if (playerLife <= 0) {
+//     playerLife = 0;
+//     reset();
+//   } else {
+//     log(
+//       `<pre>Enemy attacked ! Your life is now at <strong>${playerLife} hp</strong></pre>`
+//     );
+//   }
+//   updateLife();
+// }
 
 // STEAL LIFE
-function stealLife() {
-  if (enemyLife > 20 && playerLife < 50) {
-    let steal = document.getElementById("stealLife");
-    steal.play();
-    enemyLife -= 10;
-    playerLife += 10;
-    playerLog(
-      `<pre>You steal life ! Your life is now at <strong>${playerLife} hp</strong><pre>Enemy's life is now at <strong>${enemyLife} hp</strong></pre>`
-    );
-  } else {
-    alert(`Nah nah... you can't steal life`);
-  }
-  updateLife();
-}
+// function stealLife() {
+//   if (enemyLife > 20 && playerLife < 50) {
+//     let steal = document.getElementById("stealLife");
+//     steal.play();
+//     enemyLife -= 10;
+//     playerLife += 10;
+//     log(
+//       `<pre style='color:#d3e876;'>You steal life ! Your life is now at <strong>${playerLife} hp</strong><pre>Enemy's life is now at <strong>${enemyLife} hp</strong></pre>`
+//     );
+//     turnToEnemy();
+//     disableButtons();
+//   } else {
+//     alert(`Nah nah... you can't steal life`);
+//   }
+//   updateLife();
+// }
 
 // TAKE A POTION
 function takePotion(p) {
@@ -137,8 +192,8 @@ function takePotion(p) {
 
   if (playerLife + p > 100) {
     playerLife = 100;
-    playerLog(
-      `<pre>Glup ! You take a maxi potion, your life is now at <strong>${playerLife} hp</strong></pre>`
+    log(
+      `<pre style='color:#d3e876;'>Glup ! You take a maxi potion, your life is now at <strong>${playerLife} hp</strong></pre>`
     );
   }
 
@@ -147,8 +202,8 @@ function takePotion(p) {
     smallPotion--;
     nbPotionStock--;
     playerLife += p;
-    playerLog(
-      `<pre>Glup ! You take a mini potion, your life is now at <strong>${playerLife} hp</strong></pre>`
+    log(
+      `<pre style='color:#d3e876;'>Glup ! You take a mini potion, your life is now at <strong>${playerLife} hp</strong></pre>`
     );
   } else if (smallPotion == 0 && p == 10) {
     alert("No mini potions anymore");
@@ -216,24 +271,24 @@ function reset() {
 }
 
 // RESET BUTTON
-function reset2() {
-  if (playerLife < 100 || nbPotionStock > 0) {
-    playerLife = 100;
-    enemyLife = 100;
-    smallPotion = 2;
-    bigPotion = 2;
-    nbPotionStock = smallPotion + bigPotion;
-  } else if (enemyLife < 100) {
-    enemyLife = 100;
-    playerLife = 100;
-    smallPotion = 2;
-    bigPotion = 2;
-    nbPotionStock = smallPotion + bigPotion;
-  }
-  logger.innerHTML = ``;
-  updateLife();
-  updatePotions();
-}
+// function reset2() {
+//   if (playerLife < 100 || nbPotionStock > 0) {
+//     playerLife = 100;
+//     enemyLife = 100;
+//     smallPotion = 2;
+//     bigPotion = 2;
+//     nbPotionStock = smallPotion + bigPotion;
+//   } else if (enemyLife < 100) {
+//     enemyLife = 100;
+//     playerLife = 100;
+//     smallPotion = 2;
+//     bigPotion = 2;
+//     nbPotionStock = smallPotion + bigPotion;
+//   }
+//   logger.innerHTML = ``;
+//   updateLife();
+//   updatePotions();
+// }
 
 // MUSIC
 function playStop() {
